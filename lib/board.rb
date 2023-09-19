9099d90e82bb27084d5febcb27764810818bd568
Andrew Lewis <andrew.lewis@appfolio.com>
2023-09-19 16:58:18 -0700

    Generate and store new game pieces

--- lib/board.rb
+++ lib/board.rb
@@ -10,18 +10,31 @@ require_relative 'pieces/king'
 
 # Stores board state and actions
 class Board
+  attr_reader :board_state
+
   def initialize
     @board_state = {}
   end
 
   def build_empty_board
-    columns = [*'a'..'h']
-    8.times do |r|
-      columns.each do |c|
-        @board_state["#{c}#{r + 1}".to_sym] = nil
+    8.times do |row|
+      [*'a'..'h'].each do |col|
+        @board_state["#{col}#{row + 1}".to_sym] = nil
       end
     end
     @board_state
+  end
+
+  def place_initial_pieces
+    @board_state.each_key do |key|
+      coordinates = symbol_to_col_row_pair(key)
+      color = coordinates[1].to_i < 3 ? 'white' : 'black'
+      next if (3..6).include?(coordinates[1].to_i)
+
+      @board_state[key] = Pawn.new(color) if %w[2 7].include?(coordinates[1])
+
+      @board_state[key] = return_piece_by_column_letter(coordinates[0], color) if %w[1 8].include?(coordinates[1])
+    end
   end
 
   def display_board
@@ -50,4 +63,30 @@ class Board
     HEREDOC
     puts output_string
   end
+
+  private
+
+  def symbol_to_col_row_pair(sym)
+    sym.to_s.split('')
+  end
+
+  def return_piece_by_column_letter(column_letter, color)
+    piece = nil
+    piece = Rook.new(color) if %w[a h].include?(column_letter)
+
+    piece = Knight.new(color) if %w[b g].include?(column_letter)
+
+    piece = Bishop.new(color) if %w[c f].include?(column_letter)
+
+    piece = Queen.new(color) if column_letter == 'd'
+
+    piece = King.new(color) if column_letter == 'e'
+
+    piece
+  end
 end
+
+b = Board.new
+b.build_empty_board
+b.place_initial_pieces
+p b.board_state
