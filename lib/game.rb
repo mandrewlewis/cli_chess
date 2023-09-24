fe898e50c31d660fe1e09c1bcc6233b019e2384c
Andrew Lewis <andrew.lewis@appfolio.com>
2023-09-23 21:19:06 -0700

    Create players on user input

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,25 @@
 
 require_relative 'board'
 require_relative 'piece'
+require_relative 'player'
 
 # Manges game flow
 class Game
+  attr_reader :players
+  attr_accessor :current_player
+
   def initialize
-    true
+    @players = []
+    @board = Board.new
   end
 
-  def setup_game
-    @board = Board.new
+  def start_game
+    assign_players
+  end
+
+  def assign_players
+    2.times do |i|
+      @players << Player.new(gets.chomp, i.zero? ? 'white' : 'black')
+    end
   end
 end
