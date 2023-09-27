1e54eef7c8c1b23becdb228e608a64fc7f598b99
Andrew Lewis <andrew.lewis@appfolio.com>
2023-09-26 21:42:13 -0700

    Define valid moves for Rook

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -2,8 +2,24 @@
 
 # Rook
 class Rook < Piece
+  VECTORS = [
+    {
+      condition: nil,
+      forward: [0, 7],
+      backward: [0, -7],
+      left: [-7, 0],
+      right: [7, 0]
+    }
+  ].freeze
+
+  attr_reader :vectors
+
   def initialize(color, coordinates, board)
     super
     @icon = @color == 'white' ? '♜' : '♖'
+    @vectors = VECTORS
+    return unless color == 'black'
+
+    @vectors = @vectors.map { |hash| flip_vector(hash) }
   end
 end
