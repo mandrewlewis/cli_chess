5d5770dbe11364e09e15d4bcc7948a2b7b322a05
Andrew Lewis <andrew.lewis@appfolio.com>
2023-09-27 22:18:25 -0700

    Build out Bishop class and vectors

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
-# Bishop
+# Rook
 class Bishop < Piece
+  VECTORS = [
+    {
+      condition: nil,
+      forward_right: [7, 7],
+      forward_left: [-7, 7],
+      back_right: [7, -7],
+      back_left: [-7, -7]
+    }
+  ].freeze
+
+  attr_reader :vectors
+
   def initialize(color, coordinates, board)
     super
     @icon = @color == 'white' ? '♝' : '♗'
+    @vectors = VECTORS
+    return unless color == 'black'
+
+    @vectors = @vectors.map { |hash| flip_vector(hash) }
   end
 end
