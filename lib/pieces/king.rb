48b2c76de65661a54b858859efedd302e6f9797f
Andrew Lewis <andrew.lewis@appfolio.com>
2023-09-28 22:08:50 -0700

    Define King movement vectors

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -2,8 +2,28 @@
 
 # King
 class King < Piece
+  VECTORS = [
+    {
+      condition: nil,
+      forward: [0, 1],
+      backward: [0, -1],
+      left: [-1, 0],
+      right: [1, 0],
+      forward_right: [1, 1],
+      forward_left: [-1, 1],
+      back_right: [1, -1],
+      back_left: [-1, -1]
+    }
+  ].freeze
+
+  attr_reader :vectors
+
   def initialize(color, coordinates, board)
     super
     @icon = @color == 'white' ? '♚' : '♔'
+    @vectors = VECTORS
+    return unless color == 'black'
+
+    @vectors = @vectors.map { |hash| flip_vector(hash) }
   end
 end
