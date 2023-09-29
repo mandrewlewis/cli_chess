35fe00cd754c5229fdf620b9a9f18d47a130a445
Andrew Lewis <andrew.lewis@appfolio.com>
2023-09-28 21:28:22 -0700

    Write Knight movement tests

---
+++ spec/pieces/knight_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require './lib/game'
+require './lib/board'
+require './lib/piece'
+
+describe Knight do
+  let(:game) { Game.new }
+  let(:board) { game.board }
+  let(:w_knight) { board.find_piece(:b1) }
+  let(:b_knight) { board.find_piece(:g8) }
+
+  context 'valid moves' do
+    before do
+      w_knight.move_self(:a3, [-1, 2])
+      w_knight.move_self(:c4, [2, 1])
+      w_knight.move_self(:e5, [2, 1])
+    end
+
+    it 'forward right (w)' do
+      moved = w_knight.valid_move?(:f7)
+      expect(moved).to eql([1, 2])
+    end
+
+    it 'forward left (w)' do
+      moved = w_knight.valid_move?(:d7)
+      expect(moved).to eql([-1, 2])
+    end
+
+    it 'back right (w)' do
+      moved = w_knight.valid_move?(:f3)
+      expect(moved).to eql([1, -2])
+    end
+
+    it 'back left (w)' do
+      moved = w_knight.valid_move?(:d3)
+      expect(moved).to eql([-1, -2])
+    end
+
+    it 'right forward (w)' do
+      moved = w_knight.valid_move?(:g6)
+      expect(moved).to eql([2, 1])
+    end
+
+    it 'right back (w)' do
+      moved = w_knight.valid_move?(:g4)
+      expect(moved).to eql([2, -1])
+    end
+
+    it 'left forward (w)' do
+      moved = w_knight.valid_move?(:c6)
+      expect(moved).to eql([-2, 1])
+    end
+
+    it 'left back (w)' do
+      moved = w_knight.valid_move?(:c4)
+      expect(moved).to eql([-2, -1])
+    end
+  end
+
+  context 'invalid moves' do
+    it 'can\'t move off the board' do
+      moved = b_knight.valid_move?(:e9)
+      expect(moved).to be_falsey
+    end
+
+    it 'can\'t capture same color piece' do
+      moved = b_knight.valid_move?(:e7)
+      expect(moved).to be_falsey
+    end
+  end
+end
