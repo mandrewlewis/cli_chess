a640d31f847acfb1c9783de3ee5bc828a3fa3f6b
Andrew Lewis <andrew.lewis@appfolio.com>
2023-09-28 21:42:04 -0700

    Define Queen movement vectors

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -2,8 +2,28 @@
 
 # Queen
 class Queen < Piece
+  VECTORS = [
+    {
+      condition: nil,
+      forward: [0, 7],
+      backward: [0, -7],
+      left: [-7, 0],
+      right: [7, 0],
+      forward_right: [7, 7],
+      forward_left: [-7, 7],
+      back_right: [7, -7],
+      back_left: [-7, -7]
+    }
+  ].freeze
+
+  attr_reader :vectors
+
   def initialize(color, coordinates, board)
     super
     @icon = @color == 'white' ? '♛' : '♕'
+    @vectors = VECTORS
+    return unless color == 'black'
+
+    @vectors = @vectors.map { |hash| flip_vector(hash) }
   end
 end
